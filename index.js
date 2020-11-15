ab1fbe6c55dfb846247ee1234d5bd79f3384b5bd
Hibathulla <57030901+Hibathulla@users.noreply.github.com>
2020-11-15 11:26:51 +0530

    Initial dice challenge website

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
   var randomNumber1 = Math.random() * 6;
   var x = Math.floor(randomNumber1) + 1;
   var randomDice = "Dice" + x + ".png";
-  var randomImage = "images/" + randomDice;
+  var randomImage = "E:/Web Development/DOM/images/" + randomDice;
   var image1 = document.querySelectorAll("img")[0];
   image1.setAttribute("src", randomImage);
 
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
   var randomDice2 = "Dice" + randomNumber2 + ".png";
-  var randomImage2 = "images/" + randomDice2;
+  var randomImage2 = "E:/Web Development/DOM/images/" + randomDice2;
   var image2 = document.querySelectorAll("img")[1];
   image2.setAttribute("src", randomImage2);
   if (randomDice > randomDice2) {
